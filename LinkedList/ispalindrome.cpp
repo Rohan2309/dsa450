34cce72f8a780aad2d4094862ffdf0f24abc910b
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2021-05-30 00:59:55 +0530

    del'

--- LinkedList/ispalindrome.cpp
+++ LinkedList/ispalindrome.cpp
@@ -46,13 +46,54 @@ void display(node *head) //head by value because wea re not mod ll
     }
     cout << endl;
 }
+
+bool isPalindrome(node *head)
+{
+    node *slow = head;
+    node *fast = head;
+    while (fast != NULL && fast->next != NULL) // getting mid value
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    } //middle element in slow
+
+    //reverse from slow to last
+    node *prev = NULL;
+    node *next;
+    node *current = slow;
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    fast = head; // at the start
+    //prev is pointing at the last
+    while (prev != NULL)
+    {
+        if (fast->data != prev->data)
+        {
+            return false;
+        }
+        else
+        {
+            fast = fast->next;
+            prev = prev->next;
+        }
+    }
+    return true;
+}
 int main()
 {
     node *head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
     insertAtTail(head, 3);
+    insertAtTail(head, 2);
+    insertAtTail(head, 1);
     display(head);
+    cout << isPalindrome(head);
 
     return 0;
 }
