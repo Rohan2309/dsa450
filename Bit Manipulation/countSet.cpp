b557c102407328dd701b38a8dc5d276c119de7de
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2021-06-01 16:39:11 +0530

    bit

---
+++ Bit Manipulation/countSet.cpp
@@ -0,0 +1,24 @@
+/*
+Number of 1 Bits 
+
+Input:
+N = 6
+Output:
+2
+Explanation:
+Binary representation is '110' 
+So the count of the set bit is 2.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countSet(int n)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
