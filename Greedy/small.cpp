f7a4a034d1eeb990bd4e080c161b40f631865647
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2021-07-29 01:47:32 +0530

    gr

---
+++ Greedy/small.cpp
@@ -0,0 +1,95 @@
+/*
+Smallest subset with sum greater than all other elements
+Difficulty Level : Easy
+Last Updated : 09 Apr, 2021
+Given an array of non-negative integers. Our task is to find minimum number of elements such that their sum should be greater than the sum of rest of the elements of the array.
+Examples : 
+ 
+
+Input : arr[] = {3, 1, 7, 1}
+Output : 1
+Smallest subset is {7}. Sum of
+this subset is greater than all
+other elements {3, 1, 1}
+
+Input : arr[] = {2, 1, 2}
+Output : 2
+In this example one element is not 
+enough. We can pick elements with 
+values 1, 2 or 2, 2. In any case, 
+the minimum count is 2.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define gc getchar_unlocked
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ff              first
+#define ss              second
+//#define int             long long
+#define ll              long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> > //min heap
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+
+ 
+ 
+void FIO()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+int minElements(int arr[], int n)
+{
+    // calculating sum of array sum
+    int totSum = 0; //rest element sum
+    int ans = 0; //counter
+    int subset = 0; //subset sum
+    for (int i = 0; i < n; i++)
+        totSum += arr[i];   
+ 
+    // sort the array r.
+    sort(arr, arr + n);
+    int j = n-1; //iterator from last
+    while(true){
+        subset+=arr[j]; //taking subset with largest number
+        totSum-=arr[j]; //reducing total sum
+        j--; //decre pointer
+        ans++; //taking one one element in subset
+        if(subset>totSum){ //when subset sum is larger than remaining sum
+            break;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    FIO();
+    int arr[] = {3, 1, 7, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << minElements(arr, n) << endl;
+    return 0;
+  
+}
