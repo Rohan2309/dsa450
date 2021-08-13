a76789d1fcb376b6c21e935d5b34f70a6ffd32e4
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2021-08-13 20:48:49 +0530

    q

---
+++ Stack & Queue/qUseSt.cpp
@@ -0,0 +1,149 @@
+/*
+Queue using two Stacks 
+Easy Accuracy: 47.29% Submissions: 71522 Points: 2
+Implement a Queue using 2 stacks s1 and s2 .
+A Query Q is of 2 Types
+(i) 1 x (a query of this type means  pushing 'x' into the queue)
+(ii) 2   (a query of this type means to pop element from queue and print the poped element)
+
+Example 1:
+
+Input:
+5
+1 2 1 3 2 1 4 2
+
+Output: 
+2 3
+
+Explanation: 
+In the first testcase
+1 2 the queue will be {2}
+1 3 the queue will be {2 3}
+2   poped element will be 2 the queue 
+    will be {3}
+1 4 the queue will be {3 4}
+2   poped element will be 3.
+Example 2:
+
+Input:
+4
+1 2 2 2 1 4
+
+Output: 
+2 -1
+
+Explanation: 
+In the second testcase 
+1 2 the queue will be {2}
+2   poped element will be 2 and 
+    then the queue will be empty
+2   the queue is empty and hence -1
+1 4 the queue will be {4}.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+// #define int             long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pqb priority_queue<int>                   //max heap
+#define pqs priority_queue<int, vi, greater<int>> //min heap
+#define setbits(x) __builtin_popcountll(x)        //gives number of setbits
+#define zrobits(x) __builtin_ctzll(x)             //gives number of 0 bits
+#define mod 1000000007                            // 1e9+7  ans%mod
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n]; //dynamic array
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)                                                      //test case
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); //random shuffle
+
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //policy based ds
+
+#define deb(x) cout << #x << " " << x << endl; //for debugging
+#define fo(i, n) for (i = 0; i < n; i++)
+#define fo1(i, n) for (i = 1; i <= n; i++)
+#define Fo(i, k, n) for (i = k; i < n; i++)
+#define vii vector<pii>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+#define vi vector<int>
+#define vvi vector<vi>
+
+/*
+struct Node
+{
+    int data;
+    struct Node *left;  //pointer pointing towards left child
+    struct Node *right; //pointer for right child
+
+    Node(int val) //constructor taking data
+    {
+        data = val;
+        left = NULL; //initially
+        right = NULL;
+    }
+};
+*/
+class StackQueue
+{
+private:
+    // These are STL stacks ( http://goo.gl/LxlRZQ )
+    stack<int> s1;
+    stack<int> s2;
+
+public:
+    void push(int);
+    int pop();
+};
+
+//using 2 stacks
+void StackQueue ::push(int x)
+{
+    // Your Code
+    //s1 all elements into s2
+    while (!s1.empty())
+    {
+        s2.push(s1.top());
+        s1.pop();
+    }
+    //new element into s1
+    s1.push(x);
+    //again put into s1 from s2
+    while (!s2.empty())
+    {
+        s1.push(s2.top());
+        s2.pop();
+    }
+}
+
+//Function to pop an element from queue by using 2 stacks.
+int StackQueue ::pop()
+{
+    // Your Code
+    //ans in s1
+    int ans;
+    if (!s1.empty())
+    {
+        ans = s1.top();
+        s1.pop();
+    }
+    else
+    {
+        ans = -1;
+    }
+    return ans;
+}
+int main()
+{
+
+    return 0;
+}
