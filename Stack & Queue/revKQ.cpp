a7ab9c252e8130a5d2c5a5b148fd75c3d98c9f01
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2021-08-22 22:33:41 +0530

    q

---
+++ Stack & Queue/revKQ.cpp
@@ -0,0 +1,127 @@
+/*
+Reverse First K elements of Queue 
+Easy Accuracy: 57.92% Submissions: 34139 Points: 2
+Given an integer K and a queue of integers, we need to reverse the order of the first K elements of the queue, leaving the other elements in the same relative order.
+
+Only following standard operations are allowed on queue.
+
+enqueue(x) : Add an item x to rear of queue
+dequeue() : Remove an item from front of queue
+size() : Returns number of elements in queue.
+front() : Finds front item.
+Example 1:
+
+Input:
+5 3
+1 2 3 4 5
+
+Output: 
+3 2 1 4 5
+
+Explanation: 
+After reversing the given
+input from the 3rd position the resultant
+output will be 3 2 1 4 5.
+
+Example 2:
+
+Input:
+4 4
+4 3 2 1
+
+Output: 
+1 2 3 4
+
+Explanation: 
+After reversing the given
+input from the 4th position the resultant
+output will be 1 2 3 4.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+// #define int             long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pqb priority_queue<int>                   //max heap
+#define pqs priority_queue<int, vi, greater<int>> //min heap
+#define setbits(x) __builtin_popcountll(x)        //gives number of setbits
+#define zrobits(x) __builtin_ctzll(x)             //gives number of 0 bits
+#define mod 1000000007                            // 1e9+7  ans%mod
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n]; //dynamic array
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)                                                      //test case
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); //random shuffle
+
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //policy based ds
+
+#define deb(x) cout << #x << " " << x << endl; //for debugging
+#define fo(i, n) for (i = 0; i < n; i++)
+#define fo1(i, n) for (i = 1; i <= n; i++)
+#define Fo(i, k, n) for (i = k; i < n; i++)
+#define vii vector<pii>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+#define vi vector<int>
+#define vvi vector<vi>
+
+/*
+struct Node
+{
+    int data;
+    struct Node *left;  //pointer pointing towards left child
+    struct Node *right; //pointer for right child
+
+    Node(int val) //constructor taking data
+    {
+        data = val;
+        left = NULL; //initially
+        right = NULL;
+    }
+};
+*/
+queue<int> modifyQueue(queue<int> q, int k)
+{
+    //add code here.
+    stack<int> s;
+    //q has 1 2 3 4 5
+    for (int i = 1; i <= k; i++)
+    {
+        s.push(q.front()); //pushing k eles in stk
+        q.pop();
+    }
+    //q has 4 5
+    //s has 1 2 3
+    while (!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+    //q has 4 5 3 2 1 (as we case push from back only)
+
+    int n = q.size(); // n is 5 and k is 3
+    int m = n - k;    //as we have to insert only 4 and 5 which are 2 elements (5-3)
+    for (int i = 1; i <= m; i++)
+    {
+        int x = q.front();
+        q.pop();
+        q.push(x);
+    }
+    //q is 3 2 1 4 5
+    return q;
+}
+int main()
+{
+
+    return 0;
+}
