757978e266dc09cdac60864c288d9fe269fb4049
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2021-08-24 00:17:13 +0530

    stk

---
+++ Stack & Queue/celebrity.cpp
@@ -0,0 +1,119 @@
+/*
+The Celebrity Problem 
+Medium Accuracy: 39.46% Submissions: 82690 Points: 4
+A celebrity is a person who is known to all but does not know anyone at a party. If you go to a party of N people, find if there is a celebrity in the party or not.
+A square NxN matrix M[][] is used to represent people at the party such that if an element of row i and column j  is set to 1 it means ith person knows jth person. Here M[i][i] will always be 0.
+Note: Follow 0 based indexing.
+ 
+
+Example 1:
+
+Input:
+N = 3
+M[][] = {{0 1 0},
+         {0 0 0}, 
+         {0 1 0}}
+Output: 1
+Explanation: 0th and 2nd person both
+know 1. Therefore, 1 is the celebrity. 
+
+Example 2:
+
+Input:
+N = 2
+M[][] = {{0 1},
+         {1 0}}
+Output: -1
+Explanation: The two people at the party both
+know each other. None of them is a celebrity.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+// #define int             long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pqb priority_queue<int>                   //max heap
+#define pqs priority_queue<int, vi, greater<int>> //min heap
+#define setbits(x) __builtin_popcountll(x)        //gives number of setbits
+#define zrobits(x) __builtin_ctzll(x)             //gives number of 0 bits
+#define mod 1000000007                            // 1e9+7  ans%mod
+#define inf 1e18
+#define ps(x, y) fixed << setprecision(y) << x
+#define mk(arr, n, type) type *arr = new type[n]; //dynamic array
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)                                                      //test case
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count()); //random shuffle
+
+//typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; //policy based ds
+
+#define deb(x) cout << #x << " " << x << endl; //for debugging
+#define fo(i, n) for (i = 0; i < n; i++)
+#define fo1(i, n) for (i = 1; i <= n; i++)
+#define Fo(i, k, n) for (i = k; i < n; i++)
+#define vii vector<pii>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+#define vi vector<int>
+#define vvi vector<vi>
+
+/*
+struct Node
+{
+    int data;
+    struct Node *left;  //pointer pointing towards left child
+    struct Node *right; //pointer for right child
+
+    Node(int val) //constructor taking data
+    {
+        data = val;
+        left = NULL; //initially
+        right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int>> &M, int n)
+    {
+        // code here
+        int in[n] = {0};  //how many people knows that person
+        int out[n] = {0}; //how many people does that person knows
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (M[i][j] == 1) //when 1 that means i knows j
+                {
+                    in[j]++;  //j is known by i
+                    out[i]++; //i knows j
+                }
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (in[i] == n - 1 and out[i] == 0) //known by all person except oneself and doesn't knows anyone
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+int main()
+{
+
+    return 0;
+}
