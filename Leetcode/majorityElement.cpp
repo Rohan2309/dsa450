7a008579e96631346a8e5b94b91dfdc90926a3fd
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2021-10-25 02:22:28 +0530

    lc

---
+++ Leetcode/majorityElement.cpp
@@ -0,0 +1,60 @@
+/*
+169. Majority Element
+Easy
+
+6833
+
+294
+
+Add to List
+
+Share
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+*/
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        //moore voting algo
+        int cnt = 0;
+        int ele = 0;
+        
+        for(int num : nums){
+            if(cnt==0){ // when counter is 0
+                ele = num; // that digit will be assumed as majority element
+            }
+            if(num == ele){ //if element is that num,  then incre counter
+                cnt++;
+            }
+            else{//or if different element comes, decrease the counter
+                cnt--;
+            }
+        }
+        // at last the ele which is there, is the majority element
+        return ele;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
