dc38c391eaf277e0ff666fd5813c728382bd6746
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2021-12-14 01:21:04 +0530

    ll

---
+++ striver/day5/day5.2_midLL.cpp
@@ -0,0 +1,61 @@
+/*
+206. Reverse Linked List
+Easy
+
+9526
+
+166
+
+Add to List
+
+Share
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fastPtr = head;
+        ListNode* slowPtr = head;
+        while(fastPtr!=NULL and fastPtr->next!=NULL){
+            slowPtr = slowPtr->next;
+            fastPtr->next = fastPtr->next->next;
+        }
+        return slowPtr;
+    }
+};
+
+int main()
+{
+    
+    return 0;
+}
