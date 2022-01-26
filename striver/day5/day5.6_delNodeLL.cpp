e81d91d9b77da95d7fe431344900b268da3f8da2
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2022-01-26 23:50:39 +0530

    str

--- striver/day5/day5.6_delNodeLL.cpp
+++ striver/day5/day5.6_delNodeLL.cpp
@@ -1,13 +1,32 @@
 /*
-class Solution {
-public:
-    void deleteNode(ListNode* node) {
-        //node to del will never be tail node
-        //we can access the right side of the given node
-        node->val = node->next->val;//copying val of right node to current node
-        node->next = node->next->next;//skipping the next node
-    }
-};
+237. Delete Node in a Linked List
+Easy
+
+3823
+
+10916
+
+Add to List
+
+Share
+Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+
+It is guaranteed that the node to be deleted is not a tail node in the list.
+
+ 
+
+Example 1:
+
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+Example 2:
+
+
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
 */
 
 #include <bits/stdc++.h>
---
+++ striver/day6/day6.1_int2ll.cpp
@@ -0,0 +1,92 @@
+/*
+160. Intersection of Two Linked Lists
+Easy
+
+7779
+
+797
+
+Add to List
+
+Share
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+Custom Judge:
+
+The inputs to the judge are given as follows (your program is not given these inputs):
+
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Intersected at '8'
+Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+Example 2:
+
+
+Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Intersected at '2'
+Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+Example 3:
+
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: No intersection
+Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *h1, ListNode *h2) {
+        //base case
+        if(!h1 or !h2){
+            return NULL;
+        }
+        ListNode *d1 = h1; //dummy nodes
+        ListNode *d2 = h2;
+        while(d1 != d2){ //keep on traversing till both of them are not equal
+            //if there is no intersection nodes also, both will be null.
+            d1= d1== NULL? h2: d1->next; // if d1 reaches the end, we are shifting it to the head of 2nd ll
+            //otherwise move 1 step further
+            d2= d2== NULL? h1: d2->next; // if d2 reaches the end, we are shifting it to the head of 1st ll
+            //otherwise move 1 step further
+        }
+        return d1; //even if there is no intersection points, it will return null.
+    }
+};
+int main()
+{
+    
+    return 0;
+}
